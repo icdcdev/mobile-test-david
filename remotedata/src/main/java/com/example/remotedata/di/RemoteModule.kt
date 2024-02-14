d78f0b851ee3f22dc16b482724a2cfcb41474e99
luis david hernandez <ldhernandez_93@hotmail.com>
2024-02-13 19:50:52 -0600

    Config Retrofit

---
+++ remotedata/src/main/java/com/example/remotedata/di/RemoteModule.kt
@@ -0,0 +1,38 @@
+package com.example.remotedata.di
+
+import com.example.remotedata.api.VolksWagenApi
+import com.squareup.moshi.Moshi
+import com.squareup.moshi.adapters.Rfc3339DateJsonAdapter
+import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
+import dagger.Provides
+import okhttp3.OkHttpClient
+import retrofit2.Retrofit
+import retrofit2.converter.moshi.MoshiConverterFactory
+import java.util.Date
+import javax.inject.Singleton
+
+class RemoteModule {
+
+    @Singleton
+    @Provides
+    fun provideOkHttpClient(): OkHttpClient {
+        return OkHttpClient.Builder()
+            .build()
+    }
+
+    @Singleton
+    @Provides
+    fun provideMarvelApi(okHttpClient: OkHttpClient): VolksWagenApi {
+        val moshi = Moshi.Builder()
+            .add(Date::class.java, Rfc3339DateJsonAdapter().nullSafe())
+            .add(KotlinJsonAdapterFactory())
+            .build()
+
+        return Retrofit.Builder()
+            .baseUrl("https://5a5q7x88fb.execute-api.us-west-2.amazonaws.com")
+            .client(okHttpClient)
+            .addConverterFactory(MoshiConverterFactory.create(moshi))
+            .build()
+            .create(VolksWagenApi::class.java)
+    }
+}
