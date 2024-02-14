7289df13b4f53ddb4fe9fa1496f6274b73422cbd
luis david hernandez <ldhernandez_93@hotmail.com>
2024-02-13 20:49:12 -0600

    Create OrderEntity

---
+++ localdata/src/main/java/com/example/localdata/entities/OrderEntity.kt
@@ -0,0 +1,23 @@
+package com.example.localdata.entities
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(
+    tableName = "order"
+)
+data class OrderEntity(
+    @PrimaryKey(autoGenerate = false) var orderId: String,
+    var serviceTypeId: Int?,
+    var serviceType: String?,
+    var model: String?,
+    var startTime: String?,
+    var plates: String?,
+    var pyramidColor: String?,
+    var pyramidNumber: Int?,
+    var orderStatusId: Int?,
+    var orderStatus: String?,
+    var isAsigned: Boolean,
+    var hasFile: Boolean,
+    var urlFile: String?
+)
