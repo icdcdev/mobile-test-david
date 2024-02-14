d410ec31baf4bea6598d85de8d22fb454d75f4ca
luis david hernandez <ldhernandez_93@hotmail.com>
2024-02-13 19:45:46 -0600

    Add class MapResultError Api and test

---
+++ remotedata/src/main/java/com/example/remotedata/common/NetWorkResult.kt
@@ -0,0 +1,6 @@
+package com.example.remotedata.common
+
+sealed class NetworkResult<T> {
+    data class  NetWorkSuccess<T>(val result: T): NetworkResult<T>()
+    data class  NetworkFailure<T>(val networkError: NetworkError): NetworkResult<T>()
+}
