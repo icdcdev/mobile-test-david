9ba5e0f26849d99e970fd0976d6263ee74a0b0c7
luis david hernandez <ldhernandez_93@hotmail.com>
2024-02-20 19:10:55 -0600

    Create init design

--- app/src/main/java/com/example/ecdcchallange/MainActivity.kt
+++ app/src/main/java/com/example/ecdcchallange/MainActivity.kt
@@ -2,10 +2,32 @@ package com.example.ecdcchallange
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupWithNavController
+import com.example.ecdcchallange.databinding.ActivityMainBinding
+import com.google.android.material.tabs.TabLayout
+import com.google.android.material.tabs.TabLayoutMediator
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        setSupportActionBar(binding.toolbar)
+
+        val viewPager = binding.viewPager
+        val tabLayout = binding.tabs
+
+        val adapter = TabPagerAdapter(this)
+        viewPager.adapter = adapter
+
+        TabLayoutMediator(tabLayout, viewPager) { tab, position ->
+            tab.text = if (position == 0) "NO ASIGNADAS" else "ASIGNADAS"
+        }.attach()
+
     }
 }
